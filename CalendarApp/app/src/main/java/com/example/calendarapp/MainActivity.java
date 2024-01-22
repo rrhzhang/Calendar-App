1cf8072184e39deb152b2112e52f3309106f509d
Ronald Zhang <117058806+rrhzhang@users.noreply.github.com>
2024-01-22 13:26:49 -0500

    colors and sidebar

--- CalendarApp/app/src/main/java/com/example/calendarapp/MainActivity.java
+++ CalendarApp/app/src/main/java/com/example/calendarapp/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.calendarapp;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.navigation.NavController;
@@ -17,7 +15,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.calendarapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
- //comment
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
 
@@ -29,19 +26,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+        /* binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        }); */
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.classes, R.id.assignments, R.id.exams)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
