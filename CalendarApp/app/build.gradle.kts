37dd6d3e7680f84a30218813765a0828dfaf4ddd
Ronald Zhang <117058806+rrhzhang@users.noreply.github.com>
2024-01-22 22:20:08 -0500

    updated plugins and added firebase for push notifs

--- CalendarApp/app/build.gradle.kts
+++ CalendarApp/app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -40,7 +42,10 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("androidx.navigation:navigation-fragment:2.7.6")
     implementation("androidx.navigation:navigation-ui:2.7.6")
+    implementation("com.google.firebase:firebase-messaging:23.4.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+    implementation("com.google.firebase:firebase-analytics")
 }
--- CalendarApp/build.gradle.kts
+++ CalendarApp/build.gradle.kts
@@ -1,4 +1,19 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+
+        classpath("com.google.gms:google-services:4.4.0")
+        val nav_version = "2.7.6"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+
+    }
 }
