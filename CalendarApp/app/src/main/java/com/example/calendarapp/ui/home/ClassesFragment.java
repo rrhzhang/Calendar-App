afdeed57ee4b24e51d1109ff388b239bffed6338
Ronald Zhang <117058806+rrhzhang@users.noreply.github.com>
2024-01-24 10:32:09 -0500

    fixed fragment names, added fragment for Todo

--- CalendarApp/app/src/main/java/com/example/calendarapp/ui/home/ClassesFragment.java
+++ CalendarApp/app/src/main/java/com/example/calendarapp/ui/home/ClassesFragment.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,9 +12,8 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.calendarapp.databinding.FragmentClassesBinding;
-import com.example.calendarapp.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends Fragment {
+public class ClassesFragment extends Fragment {
 
     private FragmentClassesBinding binding;
 
@@ -23,14 +22,14 @@ public class HomeFragment extends Fragment {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
+        //TODO add click listener for button to add TextEdit to a list
+
         binding = FragmentClassesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textView;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
-
-        //textView.findViewById(R.id.class_button)
     }
 
     @Override
